bb08a8c2c5681b260b8000f95068224420623ffd
sunnyjsw92 <sunnyjsw92@gmail.com>
2022-03-09 06:19:35 -0700

    assignment 5

---
+++ assign 5/src/com/sj/assign5/Validator.java
@@ -0,0 +1,25 @@
+package com.sj.assign5;
+
+public class Validator {
+
+    public static boolean nameCheck(String inputName) {
+        String nameRegex = "^[a-zA-Z]{1,24}$";
+        return inputName.matches(nameRegex);
+    }
+
+    public static boolean phoneCheck(String inputPhone) {
+//  String phonePattern = "^\+?1-\d{3}-\d{3}-\d{4}$";
+        String phoneRegex = "^\\d{3}-\\d{3}-\\d{4}$";
+        return inputPhone.matches(phoneRegex);
+    }
+
+    public static boolean emailCheck(String inputEmail) {
+        String emailRegex = "^\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$";
+        return inputEmail.matches(emailRegex);
+    }
+
+    public static boolean ageCheck(int inputage) {
+        return inputage >= 18 && inputage <= 110;
+    }
+
+}
